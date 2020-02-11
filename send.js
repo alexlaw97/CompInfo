4a99f7a3039ddc53f8a748029b5f87dd0b8b561a
alexlaw97 <42798698+alexlaw97@users.noreply.github.com>
2020-02-11 10:19:23 +0800

    email

--- send.js
+++ send.js
@@ -1,7 +1,7 @@
 const amqp = require('amqplib/callback_api');
-const nodemailer = require('nodemailer')
-var username = 'contributor477@gmail.com';
-var password = 'Contributor123~';
+// const nodemailer = require('nodemailer')
+// var username = 'contributor477@gmail.com';
+// var password = 'Contributor123~';
 // amqp.connect('amqp://hfptilho:Bun-1UDcqu42BFT2RHnHVEkAsZYi3doP@toad.rmq.cloudamqp.com/hfptilho', function(err, conn) {
   
 //   conn.createChannel(function(err, ch) {
@@ -26,28 +26,77 @@ var password = 'Contributor123~';
 //     });
 
 
-    var transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth: {
-        user: 'contributor477@gmail.com',
-        pass: 'Contributor123~'
-      }
+    // var transporter = nodemailer.createTransport({
+    //   host: 'smtp.gmail.com',
+    //   port: 465,
+    //   secure: true,
+    //   auth: {
+    //     user: 'contributor477@gmail.com',
+    //     pass: 'Contributor123~'
+    //   }
+    // });
+    
+    // var mailOptions = {
+    //   from: username,
+    //   to: 'robotboss1997@gmail.com',
+    //   subject: 'Warning ',
+    //   text: "Hi testing 1 2"
+    // };
+    
+    // transporter.sendMail(mailOptions, function(error, info){
+    //   if (error) {
+    //     console.log(error);
+    //   } else {
+    //     console.log('Email sent: ' + info.response);
+        
+    //   }
+    // });
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: {
+        personalizations: [
+          {
+            to: [
+              {
+                email: 'weijinglaw97@gmail.com',
+              },
+            ],
+            subject: 'Hello World from the SendGrid Node.js Library!',
+          },
+        ],
+        from: {
+          email: 'app156242223@heroku.com',
+        },
+        content: [
+          {
+            type: 'text/plain',
+            value: 'Hello, Email!',
+          },
+        ],
+      },
     });
     
-    var mailOptions = {
-      from: username,
-      to: 'robotboss1997@gmail.com',
-      subject: 'Warning ',
-      text: "Hi testing 1 2"
-    };
+    //With promise
+    sg.API(request)
+      .then(response => {
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+      })
+      .catch(error => {
+        //error is an instance of SendGridError
+        //The full response is attached to error.response
+        console.log(error.response.statusCode);
+      });
     
-    transporter.sendMail(mailOptions, function(error, info){
+    //With callback
+    sg.API(request, function(error, response) {
       if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-        
+        console.log('Error response received');
       }
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(response.headers);
     });
