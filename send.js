e2a960507923d759f916c269317dde47a97d4b01
alexlaw97 <42798698+alexlaw97@users.noreply.github.com>
2020-02-11 10:23:35 +0800

    demo email

--- send.js
+++ send.js
@@ -51,52 +51,62 @@ const amqp = require('amqplib/callback_api');
         
     //   }
     // });
-    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: {
-        personalizations: [
-          {
-            to: [
-              {
-                email: 'weijinglaw97@gmail.com',
-              },
-            ],
-            subject: 'Hello World from the SendGrid Node.js Library!',
-          },
-        ],
-        from: {
-          email: 'app156242223@heroku.com',
-        },
-        content: [
-          {
-            type: 'text/plain',
-            value: 'Hello, Email!',
-          },
-        ],
-      },
-    });
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: 'test@example.com',
+        from: 'test@example.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      };
+      sgMail.send(msg);
+    // var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    // var request = sg.emptyRequest({
+    //   method: 'POST',
+    //   path: '/v3/mail/send',
+    //   body: {
+    //     personalizations: [
+    //       {
+    //         to: [
+    //           {
+    //             email: 'weijinglaw97@gmail.com',
+    //           },
+    //         ],
+    //         subject: 'Hello World from the SendGrid Node.js Library!',
+    //       },
+    //     ],
+    //     from: {
+    //       email: 'app156242223@heroku.com',
+    //     },
+    //     content: [
+    //       {
+    //         type: 'text/plain',
+    //         value: 'Hello, Email!',
+    //       },
+    //     ],
+    //   },
+    // });
     
-    //With promise
-    sg.API(request)
-      .then(response => {
-        console.log(response.statusCode);
-        console.log(response.body);
-        console.log(response.headers);
-      })
-      .catch(error => {
-        //error is an instance of SendGridError
-        //The full response is attached to error.response
-        console.log(error.response.statusCode);
-      });
+    // //With promise
+    // sg.API(request)
+    //   .then(response => {
+    //     console.log(response.statusCode);
+    //     console.log(response.body);
+    //     console.log(response.headers);
+    //   })
+    //   .catch(error => {
+    //     //error is an instance of SendGridError
+    //     //The full response is attached to error.response
+    //     console.log(error.response.statusCode);
+    //   });
     
-    //With callback
-    sg.API(request, function(error, response) {
-      if (error) {
-        console.log('Error response received');
-      }
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-    });
+    // //With callback
+    // sg.API(request, function(error, response) {
+    //   if (error) {
+    //     console.log('Error response received');
+    //   }
+    //   console.log(response.statusCode);
+    //   console.log(response.body);
+    //   console.log(response.headers);
+    // });
