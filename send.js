dce0ab03b4671208f1f1dc3b2d2aee819c7353e1
alexlaw97 <42798698+alexlaw97@users.noreply.github.com>
2020-02-11 10:57:46 +0800

    Update send.js

--- send.js
+++ send.js
@@ -51,62 +51,22 @@ const amqp = require('amqplib/callback_api');
         
     //   }
     // });
-    const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'weijinglaw97@gmail.com',
-  from: 'robotboss1997@gmail.com',
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
-    // var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    // var request = sg.emptyRequest({
-    //   method: 'POST',
-    //   path: '/v3/mail/send',
-    //   body: {
-    //     personalizations: [
-    //       {
-    //         to: [
-    //           {
-    //             email: 'weijinglaw97@gmail.com',
-    //           },
-    //         ],
-    //         subject: 'Hello World from the SendGrid Node.js Library!',
-    //       },
-    //     ],
-    //     from: {
-    //       email: 'app156242223@heroku.com',
-    //     },
-    //     content: [
-    //       {
-    //         type: 'text/plain',
-    //         value: 'Hello, Email!',
-    //       },
-    //     ],
-    //   },
-    // });
+    var helper = require('sendgrid').mail;
+    var from_email = new helper.Email('robotboss1997@gmail.com');
+    var to_email = new helper.Email('robotboss1997@gmail.com');
+    var subject = 'Hello World from the SendGrid Node.js Library!';
+    var content = new helper.Content('text/plain', 'Hello, Email!');
+    var mail = new helper.Mail(from_email, subject, to_email, content);
     
-    // //With promise
-    // sg.API(request)
-    //   .then(response => {
-    //     console.log(response.statusCode);
-    //     console.log(response.body);
-    //     console.log(response.headers);
-    //   })
-    //   .catch(error => {
-    //     //error is an instance of SendGridError
-    //     //The full response is attached to error.response
-    //     console.log(error.response.statusCode);
-    //   });
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON(),
+    });
     
-    // //With callback
-    // sg.API(request, function(error, response) {
-    //   if (error) {
-    //     console.log('Error response received');
-    //   }
-    //   console.log(response.statusCode);
-    //   console.log(response.body);
-    //   console.log(response.headers);
-    // });
+    sg.API(request, function(error, response) {
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(response.headers);
+    });
