6b8efd41a918d9d0178af764c593a04bc4b83acd
alexlaw97 <42798698+alexlaw97@users.noreply.github.com>
2020-02-11 10:32:57 +0800

    Update send.js

--- send.js
+++ send.js
@@ -51,25 +51,16 @@ const amqp = require('amqplib/callback_api');
         
     //   }
     // });
-    var helper = require('sendgrid').mail;
-    var from_email = new helper.Email('robotboss1997@gmail.com');
-    var to_email = new helper.Email('weijinglaw97@gmail.com');
-    var subject = 'Hello World from the SendGrid Node.js Library!';
-    var content = new helper.Content('text/plain', 'Hello, Email!');
-    var mail = new helper.Mail(from_email, subject, to_email, content);
-
-    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: mail.toJSON(),
-    });
-
-    sg.API(request, function(error, response) {
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-    });
+    const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'weijinglaw97@gmail.com',
+  from: 'test@example.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
     // var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
     // var request = sg.emptyRequest({
     //   method: 'POST',
