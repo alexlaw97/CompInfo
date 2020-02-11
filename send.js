3fb109d2a4812c0d11935fa3c27901bde752d59d
alexlaw97 <42798698+alexlaw97@users.noreply.github.com>
2020-02-11 11:06:58 +0800

    Update send.js

--- send.js
+++ send.js
@@ -51,22 +51,33 @@ const amqp = require('amqplib/callback_api');
         
     //   }
     // });
-    var helper = require('sendgrid').mail;
-    var from_email = new helper.Email('robotboss1997@gmail.com');
-    var to_email = new helper.Email('robotboss1997@gmail.com');
-    var subject = 'Hello World from the SendGrid Node.js Library!';
-    var content = new helper.Content('text/plain', 'Hello, Email!');
-    var mail = new helper.Mail(from_email, subject, to_email, content);
+    // var helper = require('sendgrid').mail;
+    // var from_email = new helper.Email('robotboss1997@gmail.com');
+    // var to_email = new helper.Email('robotboss1997@gmail.com');
+    // var subject = 'Hello World from the SendGrid Node.js Library!';
+    // var content = new helper.Content('text/plain', 'Hello, Email!');
+    // var mail = new helper.Mail(from_email, subject, to_email, content);
     
-    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: mail.toJSON(),
-    });
+    // var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    // var request = sg.emptyRequest({
+    //   method: 'POST',
+    //   path: '/v3/mail/send',
+    //   body: mail.toJSON(),
+    // });
     
-    sg.API(request, function(error, response) {
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-    });
+    // sg.API(request, function(error, response) {
+    //   console.log(response.statusCode);
+    //   console.log(response.body);
+    //   console.log(response.headers);
+    // });
+
+    const sgMail = require("@sendgrid/mail");
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg={
+      to:"robotboss1997@gmail.com",
+      from: "robotboss1997@gmail.com",
+      subject: "Sending Fun",
+      text: " easy",
+      html: "<strong> Well Played</strong>",
+    };
+    sgMail.send(msg);
